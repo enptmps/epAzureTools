8f8fbd3a1c79d3aec3168469b0e1aac24c32014b
Ryan Coates <ryan.coates@gmail.com>
2017-04-26 19:42:39 -0700

    started branch

--- epAzureTools/epAzureTools.psm1
+++ epAzureTools/epAzureTools.psm1
@@ -1,12 +1,24 @@
-#. $PSScriptRoot\Public\function-Enable-epAzureRemoteAccess.ps1
-#. $PSScriptRoot\Public\function-Disable-epAzureRemoteAccess.ps1
-. $PSScriptRoot\Public\function-Get-epOutsideIPAddress.ps1
-. $PSScriptRoot\Public\function-Connect-epAzureFileShare.ps1
-. $PSScriptRoot\Public\function-Get-epVMInventory.ps1
-#. $PSScriptRoot\Private\function-Add-epAzureSimpleNSGRule.ps1
-#. $PSScriptRoot\Private\funciton-Remove-epAzureSimpleNSGRule.ps1
+#Get public and private function definition files.
+    $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+    $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+    $Classes = @( Get-ChildItem -Path $PSScriptRoot\Classes\*.ps1 -ErrorAction SilentlyContinue )   
 
-#Export-ModuleMember Enable-epAzureRemoteAccess
-#Export-ModuleMember Disable-epAzureRemoteAccess
-Export-ModuleMember -Function *-*
-#Export-ModuleMember 
+#Dot source the files
+    Foreach($import in @($Public + $Private))
+    {
+        Try
+        {
+            . $import.fullname
+        }
+        Catch
+        {
+            Write-Error -Message "Failed to import function $($import.fullname): $_"
+        }
+    }
+
+# Here I might...
+    # Read in or create an initial config file and variable
+    # Export Public functions ($Public.BaseName) for WIP modules
+    # Set variables visible to the module and its functions only
+
+Export-ModuleMember -Function $Public.Basename
